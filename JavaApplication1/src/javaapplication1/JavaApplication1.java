4b244fa88f42ca81dd93151359e03d2d959ee5cd
LK09NU14UTY <LK09NU14UTY@LK09NU14>
2018-09-29 16:25:45 +0700

    belajar

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author LK09NU14UTY
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int value = 10;
+       char x;
+       x = 'A';
+        System.out.println(value);
+        System.out.println("the value of x=" + x);// TODO code application logic here
+    }
+    
+}
---
+++ JavaApplication1/src/javaapplication1/aritmatikaDemo.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author LK09NU14UTY
+ */
+public class aritmatikaDemo {
+    public static void main (String[] args)
+    {
+        int i =27;
+        int j =56;
+        double x = 19.789;
+        double y =8.29;
+        System.out.println("variabel values....");
+        System.out.println("i = "+ i);
+        System.out.println("j = "+ j);
+        System.out.println("x = "+ x);
+        System.out.println("y = "+ y);
+        //penjumlahan angka
+        System.out.println("adding....");
+        System.out.println("i + j = "+(i+j));
+        System.out.println("x + y = "+(x+y));
+        //pengurangan angka
+        System.out.println("subtrating ");
+        System.out.println("i - j = "+(i-j));
+        System.out.println("x - y = "+(x-y));
+        //perkalian angka
+        System.out.println("Multipliying....");
+        System.out.println("i * j = "+(i*j));
+        System.out.println("x * y = "+(x*y));
+        //pembagian angka
+        System.out.println("dividing....");
+        System.out.println("i / j = "+(i/j));
+        System.out.println("x / y = "+(x/y));
+        //hasil modulus
+        System.out.println("computing the remainder....");
+        System.out.println("i % j = "+(i%j));
+        System.out.println("i % j = "+(x%y));
+        //tipe pengabungan
+        System.out.println("mixing tipes....");
+        System.out.println("j + y = "+(j+y));
+        System.out.println("i * x = "+(i*x));  
+    }
+    
+}
